0d03e9d31b04ed378aaf0fad65cff68e29c8928e
Stanislav Ivanov <stanislav.ivanov9213@gmail.com>
2023-08-16 22:58:44 +0300

    feat: transform transaction model extend

--- src/lib/resources/transaction-webhook-processing-pipeline/src/transaction-remodeller/tranformed-transaction-item.model.ts
+++ src/lib/resources/transaction-webhook-processing-pipeline/src/transaction-remodeller/tranformed-transaction-item.model.ts
@@ -1,6 +1,24 @@
-export class TranformedTransactionItemModel {
+import {BankTransactionsPayload} from "../bank-transactions.types";
 
-    public async create() {
+export class TranformedTransactionItemModel {
+    private readonly PARTITION_KEY = 'PK';
+    private readonly SORT_KEY = 'SK';
+
+    private partitionKey: string;
+    private sortKey: string;
+    private iban: string;
+    private amount: number;
+    private currency: string;
+    private transactionId: string;
+    private transactionDate: string;
+    private bankName: string;
+    private otherData?: any;
+
+    public async create(transactionData: BankTransactionsPayload) {
+
+    }
+
+    protected buildPartitionKey() {
 
     }
 }
